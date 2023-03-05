e8214e0e7451c34fcb6d316e235dc57caff20483
zerrouk-ilyasse <zerrouki@b2b.regn.net>
2023-03-05 14:44:53 +0100

    fixe

--- modules/core-config/modules/kube-prometheus-stack/identity.tf
+++ modules/core-config/modules/kube-prometheus-stack/identity.tf
@@ -38,18 +38,18 @@ module "identity_thanos" {
   location            = var.location
   resource_group_name = var.resource_group_name
 
-  workload_identity = var.workload_identity && local.use_aad_workload_identity
-  oidc_issuer_url   = var.cluster_oidc_issuer_url
+  #workload_identity = var.workload_identity && local.use_aad_workload_identity
+  #oidc_issuer_url   = var.cluster_oidc_issuer_url
 
   name = "${var.cluster_name}-thanos"
-
+/*
   subjects = [
     "system:serviceaccount:${var.namespace}:${local.prometheus_service_account_name}",
     "system:serviceaccount:${var.namespace}:${local.thanos_compact_service_account_name}",
     "system:serviceaccount:${var.namespace}:${local.thanos_rule_service_account_name}",
     "system:serviceaccount:${var.namespace}:${local.thanos_store_gateway_service_account_name}"
   ]
-
+*/
   namespace = var.namespace
   labels    = var.labels
 
